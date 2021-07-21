876d3af6e98831f02ebd7adc64636f0bfd680933
Tatiana Krechowiec <tatiana60400@gmail.com>
2021-07-21 14:43:58 +0200

    precedent - suivant (page categories)

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,13 +27,21 @@ class CategoryController extends AbstractController
      * @param $productRepository
      * @return Response
      */
-    public function category($slug, ProductRepository $productRepository): Response
+    public function category($slug, ProductRepository $productRepository, Request $request, PaginatorInterface $paginator): Response
     {
+        $donnees = $this->getDoctrine()->getRepository(Product::class)->findBy([],['id' => 'desc']);
+
+        $products = $paginator->paginate(
+            $donnees,
+            $request->query->getInt('page', 1),12
+        );
+
         $category = $this->categoryRepository->findOneBy(['slug'=>$slug]);
 
         return $this->render('category.html.twig', [
             'category' => $category,
             'products'=> $productRepository->findBy(['category'=>$category]),
+            'page_products' => $products,
         ]);
     }
 }
